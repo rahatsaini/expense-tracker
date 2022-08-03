f6dd8ce64e18d036635d84a31020a6f2fe28e26a
rahat saini <rahat.saini@agriculture.gov.au>
2022-08-03 16:46:33 +1000

    added new expense

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -2,8 +2,15 @@ import NewExpenseForm from './NewExpenseForm'
 import './NewExpense.css'
 
 const NewExpense = () =>{
+  const SaveExpenseDataHandler = (expenseDataEntered) =>{
+    const expenseData ={
+      ...expenseDataEntered,
+      id: Math.random().toString(),
+    };
+    console.log(expenseData)
+  }
   return <div className="new-expense">
-    <NewExpenseForm/>
+    <NewExpenseForm  onSaveExpenseData={SaveExpenseDataHandler}/>
     </div>
 
 }
--- src/components/NewExpense/NewExpenseForm.js
+++ src/components/NewExpense/NewExpenseForm.js
@@ -1,24 +1,68 @@
+import { useState } from "react";
 import "./NewExpense.css";
 
-const NewExpenseForm = () => {
+const NewExpenseForm = (props) => {
+
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [enteredDate, setEnteredDate] = useState('');
+
+
+
+
   const titleChangeHandler = (event) => {
-    console.log(event.target.value);
-    
+    setEnteredTitle(event.target.value);
+    // setUSerInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
   };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value)
+    // setUSerInput({
+    //   ...userInput,
+    //   enteredAmount: event.target.value,
+    // });
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    // setUSerInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate)
+    };
+     props.onSaveExpenseData(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler}  placeholder="Title"/>
+          <input
+            type="text"
+            onChange={titleChangeHandler}
+            placeholder="Title"
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
-          <input type="number" min="0.01" step="1" />
+          <input type="number" min="0.01"  onChange={amountChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Date</label>
-          <input type="date" min="2019-01-01" />
+          <input type="date" min="2019-01-01"  onChange={dateChangeHandler} />
         </div>
       </div>
       <div className="new-expense__actions">
